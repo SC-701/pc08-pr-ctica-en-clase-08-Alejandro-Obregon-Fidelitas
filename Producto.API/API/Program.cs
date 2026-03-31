7a7c3c31f43e631820063ce35fb2ae1f8975c9d3
Alejandro <aobregon00445@ufide.ac.cr>
2026-03-31 14:49:12 -0600

    Incluir Seguridad en API

--- Producto.API/API/Program.cs
+++ Producto.API/API/Program.cs
@@ -1,4 +1,4 @@
-using Abstracciones.Interfaces.DA;
+﻿using Abstracciones.Interfaces.DA;
 using Abstracciones.Interfaces.Flujo;
 using Abstracciones.Interfaces.Reglas;
 using Abstracciones.Interfaces.Servicios;
@@ -8,10 +8,31 @@ using DA.Repositorios;
 using Flujo;
 using Reglas;
 using Servicios;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Autorizacion.Middleware;
+using Abstracciones.Modelos;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var tokenConfig = builder.Configuration.GetSection("Token").Get<TokenConfiguracion>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = tokenConfig.Issuer,
+            ValidAudience = tokenConfig.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                                           Encoding.UTF8.GetBytes(tokenConfig.key))
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -31,6 +52,15 @@ builder.Services.AddScoped<IConfiguracion, Configuracion>();
 builder.Services.AddScoped<ITipoCambioServicio, TipoCambioServicio>();
 builder.Services.AddScoped<IProductosReglas, ProductoReglas>();
 
+// ★ Registrar servicios del paquete de Autorización
+builder.Services.AddTransient<Autorizacion.Abstracciones.Flujo.IAutorizacionFlujo,
+                               Autorizacion.Flujo.AutorizacionFlujo>();
+builder.Services.AddTransient<Autorizacion.Abstracciones.DA.ISeguridadDA,
+                               Autorizacion.DA.SeguridadDA>();
+builder.Services.AddTransient<Autorizacion.Abstracciones.DA.IRepositorioDapper,
+                               Autorizacion.DA.Repositorios.RepositorioDapper>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -42,6 +72,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.AutorizacionClaims();
 app.UseAuthorization();
 
 app.MapControllers();
